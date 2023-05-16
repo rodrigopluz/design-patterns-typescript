7c88e47602e657aed2aa231be1435ed2b097884a
rodrigopluz <rodrigo.pluz@2ndmarket.com.br>
2023-05-15 22:07:20 -0300

    função matematica para ambos os inputs

--- src/behavioural/observer/observer.js
+++ src/behavioural/observer/observer.js
@@ -1,79 +1,82 @@
 var InputObservable = /** @class */ (function () {
-  function InputObservable(element) {
-    this.element = element;
-    this.observers = [];
-  }
-  InputObservable.prototype.registerObserver = function () {
-    var _this = this;
-    var observers = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-      observers[_i] = arguments[_i];
+    function InputObservable(element) {
+        this.element = element;
+        this.observers = [];
     }
-    observers.forEach(function (observer) {
-      if (!_this.observers.includes(observer)) {
-        _this.observers.push(observer);
-      }
-    });
-  };
-  InputObservable.prototype.removeObserver = function (observer) {
-    var observerIndex = this.observers.indexOf(observer);
-    if (observerIndex !== -1) {
-      this.observers.splice(observerIndex, 1);
-    }
-  };
-  InputObservable.prototype.notifyObserver = function () {
-    var _this = this;
-    this.observers.forEach(function (observer) {
-      return observer.update(_this);
-    });
-  };
-  return InputObservable;
-})();
+    InputObservable.prototype.registerObserver = function () {
+        var _this = this;
+        var observers = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            observers[_i] = arguments[_i];
+        }
+        observers.forEach(function (observer) {
+            if (!_this.observers.includes(observer)) {
+                _this.observers.push(observer);
+            }
+        });
+    };
+    InputObservable.prototype.removeObserver = function (observer) {
+        var observerIndex = this.observers.indexOf(observer);
+        if (observerIndex !== -1) {
+            this.observers.splice(observerIndex, 1);
+        }
+    };
+    InputObservable.prototype.notifyObserver = function () {
+        var _this = this;
+        this.observers.forEach(function (observer) { return observer.update(_this); });
+    };
+    return InputObservable;
+}());
 var ParagraphObserver = /** @class */ (function () {
-  function ParagraphObserver(element) {
-    this.element = element;
-  }
-  ParagraphObserver.prototype.update = function (observable) {
-    if (observable instanceof InputObservable) {
-      this.element.innerText = observable.element.value;
+    function ParagraphObserver(element) {
+        this.element = element;
     }
-  };
-  return ParagraphObserver;
-})();
-var DivObserver = /** @class */ (function () {
-  function DivObserver(element) {
-    this.element = element;
-  }
-  DivObserver.prototype.update = function (observable) {
-    if (observable instanceof InputObservable) {
-      this.element.innerText = observable.element.value;
-    }
-  };
-  return DivObserver;
-})();
+    ParagraphObserver.prototype.update = function (observable) {
+        if (observable instanceof InputObservable) {
+            this.element.innerText = observable.element.value;
+        }
+    };
+    return ParagraphObserver;
+}());
 function makeInput() {
-  var input = document.createElement('input');
-  document.body.appendChild(input);
-  return input;
+    var input = document.createElement('input');
+    document.body.appendChild(input);
+    return input;
 }
 function makeParagraph() {
-  var p = document.createElement('p');
-  document.body.appendChild(p);
-  p.innerText = 'Valor inicial';
-  return p;
+    var p = document.createElement('p');
+    document.body.appendChild(p);
+    p.innerText = 'Valor:';
+    return p;
 }
-function makeDiv() {
-  var div = document.createElement('div');
-  document.body.appendChild(div);
-  div.innerText = 'Texto da DIV';
-  return div;
-}
-var input = new InputObservable(makeInput());
+var input_val1 = new InputObservable(makeInput());
 var p1 = new ParagraphObserver(makeParagraph());
+var input_val2 = new InputObservable(makeInput());
 var p2 = new ParagraphObserver(makeParagraph());
-var div1 = new DivObserver(makeDiv());
-input.registerObserver(p1, p2, div1);
-input.element.addEventListener('keyup', function () {
-  input.notifyObserver();
+input_val1.registerObserver(p1);
+input_val2.registerObserver(p2);
+var result = makeParagraph();
+result.setAttribute('id', 'result');
+result.innerText = 'Resultado: ';
+// input 1
+input_val1.element.setAttribute('id', 'input_val1');
+input_val1.element.setAttribute('type', 'number');
+input_val1.element.setAttribute('placeholder', 'Valor 1');
+input_val1.element.addEventListener('keyup', function () {
+    input_val1.notifyObserver();
 });
-input.removeObserver(p2);
+var resp = 0;
+if (resp !== undefined || resp !== 'NaN') {
+    // input 2
+    input_val2.element.setAttribute('id', 'input_val2');
+    input_val2.element.setAttribute('type', 'number');
+    input_val2.element.setAttribute('placeholder', 'Valor 2');
+    input_val2.element.addEventListener('keyup', function () {
+        input_val2.notifyObserver();
+        var val_input_1 = input_val1.element.value;
+        var val_input_2 = input_val2.element.value;
+        resp = parseInt(val_input_1) / parseInt(val_input_2);
+        // result
+        result.innerText = 'Resultado: ' + resp.toString();
+    });
+}
--- src/behavioural/observer/observer.ts
+++ src/behavioural/observer/observer.ts
@@ -43,15 +43,6 @@ class ParagraphObserver implements Observer {
   }
 }
 
-class DivObserver implements Observer {
-  constructor(public element: HTMLDivElement) {}
-  update(observable: Subject): void {
-    if (observable instanceof InputObservable) {
-      this.element.innerText = observable.element.value;
-    }
-  }
-}
-
 function makeInput(): HTMLInputElement {
   const input = document.createElement('input');
   document.body.appendChild(input);
@@ -61,25 +52,46 @@ function makeInput(): HTMLInputElement {
 function makeParagraph(): HTMLParagraphElement {
   const p = document.createElement('p');
   document.body.appendChild(p);
-  p.innerText = 'Valor inicial';
+  p.innerText = 'Valor:';
   return p;
 }
 
-function makeDiv(): HTMLDivElement {
-  const div = document.createElement('div');
-  document.body.appendChild(div);
-  div.innerText = 'Texto da DIV';
-  return div;
-}
-
-const input = new InputObservable(makeInput());
+const input_val1 = new InputObservable(makeInput());
 const p1 = new ParagraphObserver(makeParagraph());
-const p2 = new ParagraphObserver(makeParagraph());
-const div1 = new DivObserver(makeDiv());
 
-input.registerObserver(p1, p2, div1);
-input.element.addEventListener('keyup', function () {
-  input.notifyObserver();
+const input_val2 = new InputObservable(makeInput());
+const p2 = new ParagraphObserver(makeParagraph());
+
+input_val1.registerObserver(p1);
+input_val2.registerObserver(p2);
+
+const result = makeParagraph();
+result.setAttribute('id', 'result');
+result.innerText = 'Resultado: ';
+
+// input 1
+input_val1.element.setAttribute('id', 'input_val1');
+input_val1.element.setAttribute('type', 'number');
+input_val1.element.setAttribute('placeholder', 'Valor 1');
+input_val1.element.addEventListener('keyup', function () {
+  input_val1.notifyObserver();
 });
 
-input.removeObserver(p2);
+let resp = 0;
+if (resp !== undefined || resp !== 'NaN') {
+  // input 2
+  input_val2.element.setAttribute('id', 'input_val2');
+  input_val2.element.setAttribute('type', 'number');
+  input_val2.element.setAttribute('placeholder', 'Valor 2');
+  input_val2.element.addEventListener('keyup', function () {
+    input_val2.notifyObserver();
+
+    const val_input_1 = input_val1.element.value;
+    const val_input_2 = input_val2.element.value;
+
+    resp = parseInt(val_input_1) / parseInt(val_input_2);
+
+    // result
+    result.innerText = 'Resultado: ' + resp.toString();
+  });
+}
